133a8f755a1e6921c7e7cf902af8964611f46b60
brandonneo <1902694@sit.singaporetech.edu.sg>
2021-11-28 23:36:49 +0800

    -1 to -2

--- GumballMachine.php
+++ GumballMachine.php
@@ -3,16 +3,16 @@
 class GumballMachine {
 
 	private $gumballs;
-	
+
 	public function getGumballs() {
 		return $this->gumballs;
 	}
-	
+
 	public function setGumballs($amount) {
 		$this->gumballs = $amount;
 	}
-	
+
 	public function turnWheel() {
-		$this->setGumballs($this->getGumballs()-1);
+		$this->setGumballs($this->getGumballs()-2);
 	}
 }
